46b266f1b3943fb40bec65e28a3263bb8df85b8c
gptmarker <gptmarker@gmail.com>
2022-12-09 13:33:58 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,44 +1,29 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <meta name="description" content="Save and share your ChatGPT conversations with GPTMarker, a Chrome extension. Easily revisit past conversations and enhance your ChatGPT experience."/>
-        <meta
-          property="twitter:image"
-          content={`${process.env.NEXT_PUBLIC_DOMAIN}chatgptog.png`}
-        />
-        <meta
-          property="og:image"
-          content={`${process.env.NEXT_PUBLIC_DOMAIN}chatgptog.png`}
-        />
-        <meta
-          property="og:title"
-          content="GPTMarker - Save and share your ChatGPT conversations"
-        />
-
-        <meta property="og:site_name" content="GPTMarker" />
-        <meta
-          property="og:description"
-          content="Save your ChatGPT threads and share with your friends."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="twitter:title" content="GPTMarker - Save and share your ChatGPT conversations" />
-        <meta
-          property="twitter:description"
-          content="Save your ChatGPT threads and share with your friends."
-        />
-        <meta property="twitter:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@gptmarker" />
-        <meta name="twitter:creator" content="Altogic" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html>
+			<Head>
+				<meta
+					name="description"
+					content="Save and share your ChatGPT conversations with GPTMarker, a Chrome extension. Easily revisit past conversations and enhance your ChatGPT experience."
+				/>
+				<meta property="og:title" content="GPTMarker - Save and share your ChatGPT conversations" />
+				<meta property="og:site_name" content="GPTMarker" />
+				<meta property="og:description" content="Save your ChatGPT threads and share with your friends." />
+				<meta property="og:type" content="website" />
+				<meta property="twitter:title" content="GPTMarker - Save and share your ChatGPT conversations" />
+				<meta property="twitter:description" content="Save your ChatGPT threads and share with your friends." />
+				<meta property="twitter:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
+				<meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@gptmarker" />
+				<meta name="twitter:creator" content="Altogic" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
