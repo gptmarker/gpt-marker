d33d085cb46377255260c34585c84546fcd210be
gptmarker <gptmarker@gmail.com>
2022-12-09 13:26:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Links from "../components/Links";
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Links from '../components/Links';
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <div className="bg-[#75A99C]">
-      <div className="container mx-auto px-4 flex flex-col items-center justify-center min-h-screen">
-        <Header />
-        <Links />
-        <section className="py-4 flex flex-col items-center justify-center w-full flex-1 px-4 text-center bg-[#75A99C]">
-          <div className="flex items-center flex-col">
-            <div className="flex items-center justify-center pt-2">
-              <p className="text-xs text-gray-700">
-                Give us a <span className="font-bold">star</span> on GitHub
-              </p>
-            </div>
-          </div>
-        </section>
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-[#75A99C]">
+			<Head>
+				<meta property="twitter:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}chatgptog.png`} />
+				<meta property="og:image" content={`${process.env.NEXT_PUBLIC_DOMAIN}chatgptog.png`} />
+			</Head>
+			<div className="container mx-auto px-4 flex flex-col items-center justify-center min-h-screen">
+				<Header />
+				<Links />
+				<section className="py-4 flex flex-col items-center justify-center w-full flex-1 px-4 text-center bg-[#75A99C]">
+					<div className="flex items-center flex-col">
+						<div className="flex items-center justify-center pt-2">
+							<p className="text-xs text-gray-700">
+								Give us a <span className="font-bold">star</span> on GitHub
+							</p>
+						</div>
+					</div>
+				</section>
+				<Footer />
+			</div>
+		</div>
+	);
 }
