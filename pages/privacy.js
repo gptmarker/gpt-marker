d2a9d6ec261c81c34b3a11fa280bbffd29404223
zinedkaloc <deniz@altogic.com>
2022-12-09 14:23:24 +0300

    privacy page added to ensure chrome marketstore extension standarts

---
+++ pages/privacy.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const privacy = () => {
+	return (
+		// create privacy page template
+		<section>
+			<h1>Privacy</h1>
+			<p>
+				Privacy Policy At "gptmarker - save and share chatgpt conversation", we are committed to protecting the
+				privacy of our users. We do not collect any personal information about you, nor do we store any
+				information about your conversations or avatar image.
+			</p>
+			<p>
+				When you use our service, we may collect and store the conversations and avatar image you create and
+				share using our service. This information is only used to provide the service to you, and is not shared
+				with any third parties.
+			</p>
+			<p>
+				If you have any questions or concerns about our privacy policy, please contact us at
+				contact@gptmarker.com.
+			</p>
+			<p>
+				By using our service, you consent to our privacy policy. This policy may be updated from time to time,
+				so please check back periodically for any changes.
+			</p>
+		</section>
+	);
+};
+
+export default privacy;
