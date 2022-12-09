0d1307562806eedbf785325f6aa8274a1672434a
zinedkaloc <deniz@altogic.com>
2022-12-09 14:31:12 +0300

    privacy policy updated

--- pages/privacy.js
+++ pages/privacy.js
@@ -6,14 +6,15 @@ const privacy = () => {
 		<section>
 			<h1>Privacy</h1>
 			<p>
-				Privacy Policy At "gptmarker - save and share chatgpt conversation", we are committed to protecting the
-				privacy of our users. We do not collect any personal information about you, nor do we store any
-				information about your conversations or avatar image.
+				Privacy Policy At "GPTMarker - save and share ChatGPT conversation", we are committed to protecting the
+				privacy of our users. We do not collect any personal information about you, we store only the
+				information about your conversations and avatar image.
 			</p>
 			<p>
-				When you use our service, we may collect and store the conversations and avatar image you create and
-				share using our service. This information is only used to provide the service to you, and is not shared
-				with any third parties.
+				When you use our service, we may collect and store the conversations and avatar image which you create
+				and share using GPTMarker. This information is only used to provide the service to you, and is not
+				shared with any third parties. If you have questions about how it is collected, please see the open
+				source code for this extension.
 			</p>
 			<p>
 				If you have any questions or concerns about our privacy policy, please contact us at
