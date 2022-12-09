ae80e80695481ad232d9e9dd69003a03ea2d9242
gptmarker <gptmarker@gmail.com>
2022-12-09 13:33:54 +0300

    dynamic og generater updated

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,83 +1,108 @@
-import React from 'react'
+import { ImageResponse } from '@vercel/og';
 
-export default function og() {
-  return (
-    <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          backgroundColor: "#F7F7F8",
-          borderRadius: "0px",
+export const config = {
+	runtime: 'experimental-edge'
+};
 
-        }}
-      >
-        <div tw="flex p-8 bg-[#F7F7F8] w-full">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://www.gptmarker.com/chatgpt.png"
-            width="48"
-            height="48"
-            alt="GPTMarker Avatar"
-            tw="rounded-md mr-4 mt-4"
-          />
-          <p
-            style={{
-              color: "#00G",
-              fontSize: "30px",
-              whiteSpace: "pre-wrap",
+const getGTPThread = async shareId => {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_ENV_URL}/thread?shareId=${shareId}`);
+	return res.json();
+};
 
-            }}
-          >
-            First question to ChatGPT asked by users to abuse the questions and fill the questions they can add more long questions to here?
-          </p>
-        </div>
-        <div tw="flex pl-8 py-8 bg-[#fff] w-full ">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="https://www.gptmarker.com/chatgpt.png"
-            width="48"
-            height="48"
-            alt="GPTMarker Avatar"
-            tw="rounded-md mr-4 mt-4"
-          />
-          <div
-            style={{
-              color: "black",
-              fontSize: "30px",
-              whiteSpace: "pre-wrap",
-              lineHeight: "42px",
-              paddingTop:"12px"
-            }}
-          >
-            First question to ChatGPT asked by users to abuse the questions and fill the questions they can add more long questions to here?
-          </div>
+export default async function handle(req) {
+	const { searchParams } = new URL(req.url || '');
+	const shareId = searchParams.get('shareId');
 
-        </div>
-        <div tw="flex items-center h-10 px-4 py-2 absolute bottom-0 right-0 bg-[#363543]/55 rounded-tl-md">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-           <img
-            src="https://www.gptmarker.com/chatgpt.png"
-            width="16"
-            height="16"
-            alt="GPTMarker - "
-            tw="rounded-sm mr-2"
-          />
-          <p
-            style={{
-              fontSize: "14px",
-              fontWeight: "bold",
-              color: "white",
-              marginTop: "16px",
-            }}
-          >
-            GPTMarker - Save and share your ChatGPT conversations
-          </p>
-        </div>
+	if (!shareId) {
+		return new Response('Missing slug', { status: 400 });
+	}
 
-      </div>
-  )
+	const gtpThread = await getGTPThread(shareId);
+
+	if (!gtpThread) {
+		return new Response('Not found', { status: 404 });
+	}
+
+	return new ImageResponse(<OgImage question={gtpThread.question} answer={gtpThread.answer} />, {
+		width: 1200,
+		height: 627
+	});
+}
+
+function OgImage({ question, answer }) {
+	return (
+		<div
+			style={{
+				height: '100%',
+				width: '100%',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'flex-start',
+				backgroundColor: '#F7F7F8',
+				borderRadius: '0px'
+			}}
+		>
+			<div tw="flex p-8 bg-[#F7F7F8] w-full">
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img
+					src="https://www.gptmarker.com/chatgpt.png"
+					width="48"
+					height="48"
+					alt="GPTMarker Avatar"
+					tw="rounded-md mr-4 mt-4"
+				/>
+				<p
+					style={{
+						color: '#00G',
+						fontSize: '30px',
+						whiteSpace: 'pre-wrap'
+					}}
+				>
+					{question}
+				</p>
+			</div>
+			<div tw="flex pl-8 py-8 bg-[#fff] w-full ">
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img
+					src="https://www.gptmarker.com/chatgpt.png"
+					width="48"
+					height="48"
+					alt="GPTMarker Avatar"
+					tw="rounded-md mr-4 mt-4"
+				/>
+				<div
+					style={{
+						color: 'black',
+						fontSize: '30px',
+						whiteSpace: 'pre-wrap',
+						lineHeight: '42px',
+						paddingTop: '12px'
+					}}
+				>
+					{answer}
+				</div>
+			</div>
+			<div tw="flex items-center h-10 px-4 py-2 absolute bottom-0 right-0 bg-[#363543]/55 rounded-tl-md">
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img
+					src="https://www.gptmarker.com/chatgpt.png"
+					width="16"
+					height="16"
+					alt="GPTMarker - "
+					tw="rounded-sm mr-2"
+				/>
+				<p
+					style={{
+						fontSize: '14px',
+						fontWeight: 'bold',
+						color: 'white',
+						marginTop: '16px'
+					}}
+				>
+					GPTMarker - Save and share your ChatGPT conversations
+				</p>
+			</div>
+		</div>
+	);
 }
